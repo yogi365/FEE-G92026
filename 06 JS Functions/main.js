cdfe09526333d24c27231e6960fa6f987a6413b9
Yogendra <sharma.yogendra@yahoo.com>
2026-02-09 18:52:57 +0530

    JS Functions

---
+++ 06 JS Functions/main.js
@@ -0,0 +1,57 @@
+// function functionName(){
+//     console.log("Inside a function")
+// }s
+
+// functionName();
+
+
+// function sum(a,b){
+//     console.log(a+b);
+// }
+
+// sum(12,13)
+
+// sum(4,8)
+// var b = 20
+// var a = sum;
+// console.log(a)
+// sum(2,6)
+
+// a(2,6)
+
+//function expression
+// var sum = function(a,b){
+//     console.log(a+b)
+// }
+
+// console.log(sum)
+// sum(2,6);
+
+
+
+// function sum(a,b){
+//     return a+b
+// }
+// console.log(sum)
+
+// function sum(a,b,c){
+//     return a+b+c;
+// }
+
+// var sum = function(a,b,c){
+//     return a+b+c;
+// }
+
+// var sum = 10;
+
+// console.log(sum);
+
+
+// var sum = (a,b)=>a+b
+// console.log(sum(2,9))
+
+var squareOfANumber = a => {
+    a *= 2;
+    return a * a;
+}
+console.log(squareOfANumber(4))
